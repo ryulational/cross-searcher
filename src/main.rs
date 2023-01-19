7987a27a90b0879d6786ad283ace9cdb1a55d523
ryulational <97367422+ryulational@users.noreply.github.com>
2023-01-18 21:48:22 -0800

    Define search engines

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,37 @@ struct Cli {
     query: String,
 }
 
+struct SearchEngine {
+    name: String,
+    pattern: String,
+}
+
 fn main() {
     let cli = Cli::parse();
     println!("Your search query: {:?}", cli.query);
-    let items = vec!["Bing", "DuckDuckGo", "Google", "Yahoo!"];
+
+    let bing: SearchEngine = SearchEngine {
+        name: String::from("Bing"),
+        pattern: String::from("https://www.bing.com/search?q="),
+    };
+
+    let duckduckgo: SearchEngine = SearchEngine {
+        name: String::from("DuckDuckgo"),
+        pattern: String::from("https://duckduckgo.com/?q="),
+    };
+
+    let google = SearchEngine {
+        name: String::from("Google"),
+        pattern: String::from("https://www.google.com/search?q="),
+    };
+
+    let yahoo: SearchEngine = SearchEngine {
+        name: String::from("Yahoo!"),
+        pattern: String::from("https://search.yahoo.com/search?p="),
+    };
+
+    let engines: Vec<SearchEngine> = vec![bing, duckduckgo, google, yahoo];
+    let items: Vec<String> = engines.into_iter().map(|x| x.name).collect();
     let search: Vec<usize> = MultiSelect::new().items(&items).interact().unwrap();
     println!("{:?}", search);
 
